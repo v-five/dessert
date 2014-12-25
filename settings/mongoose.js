f2adc9366f8d2de4638df7de1f0ae097aef4487a
v-five <vityka.vv@gmail.com>
2014-12-25 22:45:43 +0200

    Removed localhost for db

--- settings/mongoose.js
+++ settings/mongoose.js
@@ -1,12 +1,12 @@
 module.exports = function(mongoose){
 
 	var db  = require('../config').database;
-	var env = process.env.NODE_ENV || 'development';
-
-	if ('development' == env) {
-		return mongoose.connect(db.url.localhost);
-	}else{
-		return mongoose.connect(db.url.mongolab);
-	}
+	//var env = process.env.NODE_ENV || 'development';
+	return mongoose.connect(db.url.mongolab);
+	//if ('development' == env) {
+	//	return mongoose.connect(db.url.localhost);
+	//}else{
+	//	return mongoose.connect(db.url.mongolab);
+	//}
 
 };

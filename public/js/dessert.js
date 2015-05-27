ec9046c0f466fe2dfbeb6493375afe5fe2a6a6dd
v-five <vityka.vv@gmail.com>
2015-05-28 00:17:34 +0300

    UI fixes

--- public/js/dessert.js
+++ public/js/dessert.js
@@ -4,4 +4,16 @@
 		    $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|file|blob):|data:image\//);
 		    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|tel|file|blob):/)
 		});
+
+		leftMenuToggle();
 })();
+
+
+function leftMenuToggle(){
+	$(document).on('click','.toggle-side-menu', function(e){
+		e.preventDefault();
+		$('left-menu').toggleClass('show');
+	}).on('click','left-menu a', function(){
+		$(this).parents('left-menu').removeClass('show');
+	})
+}
